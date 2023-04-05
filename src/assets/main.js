fd42f3debfb7017a4426acce536981fd8a6cc375
Gustavo Aguilar <gustavoalonsoaa82@gmail.com>
2023-04-05 15:22:00 -0600

    [ADD] Redirect to the youtube videos using their API id

--- src/assets/main.js
+++ src/assets/main.js
@@ -22,7 +22,10 @@ async function fetchData(urlApi) {
       const videos = await fetchData(API);
       let view = `
       ${videos.items.map(video => `
-        <div class="group relative">
+        <div class="group relative" 
+          title="Open in a new tab"
+          onclick="window.open('https://www.youtube.com/watch?v=${video.id.videoId}', '_blank')">
+          
           <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
             <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
